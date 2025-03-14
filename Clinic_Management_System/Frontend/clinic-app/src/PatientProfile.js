9cd61a6c2711efb10c6177c3a8ddd29e68703a1a
omargamal7688 <omar.gamal@oneteam.plus>
2025-03-14 04:11:47 +0200

    remove front end

--- Clinic_Management_System/Frontend/clinic-app/src/PatientProfile.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-const PatientProfile = () => {
-  <h1>Patient Profile</h1>
-};
-
-export default PatientProfile;
